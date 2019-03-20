a8b8aee31885774340281fa7489255d45ce6fc39
Lucy Holubova <lucy.holubova90@gmail.com>
2019-03-20 15:30:18 +0000

    Pull portfolios into the edit project page
    This commit will:
    - use the pullPortfolio function to get the projects into the edit project html
    - select the correct portfolio based on the id
    - pass the correct values into the edit project fields

--- portfolio-edit/edit-project.php
+++ portfolio-edit/edit-project.php
@@ -1,18 +1,31 @@
 <?php
+
+include './pullPortfolio.php';
+
 $id = $_GET['id'];
+
+$projects = pullPortfolio();
+$currentProject;
 ?>
 
 <!DOCTYPE html>
+<?php
 
+foreach($projects as $project){
+    if ($project['id'] === $id) {
+        $currentProject = $project;
+    }
+}
+?>
 <form method="POST" action="add-new-project.php">
     <label for="pname">Project Name</label>
-    <input type="text" name="pname" id="pname">
+    <input value="<?= $project['name'] ?>" type="text" name="pname" id="pname">
 
     <label for="url">Project Url</label>
-    <input type="text" name="url" id="url">
+    <input value="<?= $project['url'] ?>" type="text" name="url" id="url">
 
     <label for="comment">Project comment</label>
-    <input type="text" name="comment" id="comment">
+    <input value="<?= $project['comment'] ?>" type="text" name="comment" id="comment">
 
     <input type="submit">
 
