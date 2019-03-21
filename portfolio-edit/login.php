7c85d2b6a259ec612d604ecf0d0426963ea08ac6
Lucy Holubova <lucy.holubova90@gmail.com>
2019-03-21 17:14:02 +0000

    made login form
    it checks with a hashed password for the only user in the db

---
+++ portfolio-edit/login.php
@@ -0,0 +1,28 @@
+<?php
+//get the login info from the form
+//get info from POST
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+//need to get the user name and password hashed from the data
+$db = new PDO('mysql:host=192.168.20.20;dbname=Project_2', 'root', '');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$sql = 'SELECT `id`, `user_name`, `password` FROM `users` WHERE `user_name` = :username;';
+$query = $db->prepare($sql);
+$query->execute([":username"=>$username]);
+$user = $query->fetch();
+//if the user doesnt exist we should error
+if ($user){
+    //if it exist then we need to compare password from the form and the password from the data
+    $passwordMatches = password_verify($password, $user['password']);
+    if ($passwordMatches){
+        //if correct we are logged in to admin page
+        header('Location: panel.php');
+    } else{
+    //if not correct throw the error
+        echo 'incorrect username or password';
+    }
+
+}else{
+    echo 'incorrect username or password';
+}
